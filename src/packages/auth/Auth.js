328d6584815dd16e1af9d40ea17e58c74de35970
nikola-wd <nikola_wd@hotmail.com>
2017-05-22 22:13:51 +0200

    added getToken fn

--- src/packages/auth/Auth.js
+++ src/packages/auth/Auth.js
@@ -4,22 +4,37 @@ export default function(Vue) {
 	Vue.auth = {
 		// set token
 		setToken: (token, expiration) => {
-			localStorage.setItem('token', token)
-			localStorage.setItem('expiration', expiration)
+			localStorage.setItem('token', token);
+			localStorage.setItem('expiration', expiration);
 		},
+
 
 
 		// get token
 		getToken: () => {
+			var token 	   = localStorage.getItem('token');
+			var expiration = localStorage.getItem('expiration');
 
+			if(!token || !expiration) {
+				return null;
+			}
+
+			if(Date.now() > parseInt(expiration)) {
+				this.destroyToken();
+				return null;
+			} else {
+				return token;
+			}
 		},
+
 
 
 		// destroy token
 		destroyToken: () => {
-			localStorage.removeItem('token')
-			localStorage.removeItem('expiration')
+			localStorage.removeItem('token');
+			localStorage.removeItem('expiration');
 		},
+
 
 
 		// isAuthenticated
@@ -33,11 +48,15 @@ export default function(Vue) {
 	} // Vue.auth
 
 
+
+
+
+
 	Object.defineProperties(Vue.prototype, {
 		$auth: {
 			get: () => {
 				return Vue.auth
 			}
 		}
-	})
+	});
 }
